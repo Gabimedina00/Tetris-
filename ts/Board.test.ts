c69be4b7c04acd34da0af33bb8267460c2ce84e6
fabriziojara181-droid <fabriziojara.181@gmail.com>
2026-09-02 05:24:12 -0300

    arreglo del aviso "Object is possibly 'undefined'" solucion "?."

--- ts/Board.test.ts
+++ ts/Board.test.ts
@@ -4,6 +4,6 @@ import { Board } from "../src/Board.js";
 it("Un tablero nuevo tiene 20 filas y 10 columnas vacias", () => {
     const board = new Board();
     expect(board.grid.length).toBe(20);
-    expect(board.grid[0].length).toBe(10);
-    expect(board.grid[0][0]).toBeNull()
+    expect(board.grid[0]?.length).toBe(10);
+    expect(board.grid[0]?.[0]).toBeNull();
     })
